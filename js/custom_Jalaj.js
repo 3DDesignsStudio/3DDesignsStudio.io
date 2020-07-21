d885457ca37d8fd3ff1ca9346ce2bc6f49321ab2
Jalaj <jalaj.purohit@gmail.com>
2020-07-21 17:47:55 +0530

    js

---
+++ js/custom_Jalaj.js
@@ -0,0 +1,106 @@
+var hero = $(".hero_area");
+var menu = $(".custom_menu-container");
+$(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > hero.height()) {
+        menu.addClass("menu_fixed-position");
+        $(".custom_menu-container + section").addClass("mt-5");
+    } else {
+        menu.removeClass("menu_fixed-position");
+        $(".custom_menu-container + section").removeClass("mt-5");
+    }
+});
+
+
+var myCircle = Circles.create({
+    id: "circles-1",
+    radius: 65,
+    value: 85,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+var myCircle = Circles.create({
+    id: "circles-2",
+    radius: 65,
+    value: 55,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+var myCircle = Circles.create({
+    id: "circles-3",
+    radius: 65,
+    value: 65,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+var myCircle = Circles.create({
+    id: "circles-4",
+    radius: 65,
+    value: 85,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+function myFunction() {
+    $(document).ready(() => {
+
+        $('video').attr('controlsList', 'nodownload');
+        
+        });
+        
+    if($("#password").val() != '3DDesign' ){
+                     alert('Please enter valid password.');
+                     $("#password").focus()
+                     return false;
+                 }
+  
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
