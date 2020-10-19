33d98e3e8ae5cfe1c85be27a0713882895ef5f28
Jalaj <jalaj.purohit@gmail.com>
2020-10-19 14:19:21 +0530

    https

--- js/custom_Jalaj.js
+++ js/custom_Jalaj.js
@@ -128,4 +128,10 @@ function myFunction() {
          
   }
 
+  checkHttpReq()
+function checkHttpReq(){	
+	if (location.protocol == 'http:'){
+	  location.href = location.href.replace("http","https");
+	}
+}
   
